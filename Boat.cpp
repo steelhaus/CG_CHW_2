6ea89ff8d18b535553abf7d861fadc1d3cccd64b
atochilin@ibs.ru <atochilin@ibs.ru>
2017-04-11 19:53:52 +0300

    unnecessary changes

--- Boat.cpp
+++ Boat.cpp
@@ -219,7 +219,7 @@ glm::vec2 vSkeletonBortsTexCoords[] = {
 	glm::vec2(0.5f,0.5f), glm::vec2(0.5f,0.725f), glm::vec2(0.0f,0.5f), glm::vec2(0.0f,0.725f),
 };
 glm::vec3 vSkeletonBortsNormals[] = {
-	glm::vec3(0.0f, 0.0f, 1.0f),
+	glm::vec3(1.0f,1.0f,1.0f),
 };
 
 /*--------------------------------------skeleton deck --------------------------------------*/
